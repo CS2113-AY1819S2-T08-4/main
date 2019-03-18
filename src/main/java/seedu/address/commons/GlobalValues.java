ae747ceb9d496c471122abefd329300e7d7fa1cb
weizhang05 <chanweizhang05@hotmail.com>
2019-03-18 17:31:02 +0800

    created GlobalValues for commonly used Strings

---
+++ src/main/java/seedu/address/commons/GlobalValues.java
@@ -0,0 +1,9 @@
+package seedu.address.commons;
+
+public class GlobalValues {
+
+    public final static String PARTICIPANTS = "PARTICIPANTS";
+    public final static String GROUP = "GROUP";
+    public final static String HOUSE = "HOUSE";
+
+}
