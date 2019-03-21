844a344582002271a1b34b82c132908a22524ddf
weizhang05 <chanweizhang05@hotmail.com>
2019-03-21 16:50:48 +0800

    temporarily removed GlobalValues

--- src/main/java/seedu/address/commons/GlobalValues.java
+++
@@ -1,9 +0,0 @@
-package seedu.address.commons;
-
-public class GlobalValues {
-
-    public final static String PARTICIPANT = "PARTICIPANT";
-    public final static String GROUP = "GROUP";
-    public final static String HOUSE = "HOUSE";
-
-}
