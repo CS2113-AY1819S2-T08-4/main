bf1f51e04909560c0c08312258a9461346f1eb4a
weizhang05 <chanweizhang05@hotmail.com>
2019-04-14 04:24:58 +0800

    update email regex to include TLD with minimum of 2 letters

--- src/main/java/seedu/address/model/participant/Email.java
+++ src/main/java/seedu/address/model/participant/Email.java
@@ -10,7 +10,7 @@ import static seedu.address.commons.util.AppUtil.checkArgument;
 public class Email {
 
     private static final String SPECIAL_CHARACTERS = "!#$%&'*+/=?`{|}~^.-";
-    public static final String MESSAGE_CONSTRAINTS = "Emails should be of the format local-part@domain "
+    public static final String MESSAGE_CONSTRAINTS = "Emails should be of the format local-part@domain.tld "
             + "and adhere to the following constraints:\n"
             + "1. The local-part should only contain alphanumeric characters and these special characters, excluding "
             + "the parentheses, (" + SPECIAL_CHARACTERS + ") .\n"
@@ -18,14 +18,19 @@ public class Email {
             + "The domain name must:\n"
             + "    - be at least 2 characters long\n"
             + "    - start and end with alphanumeric characters\n"
-            + "    - consist of alphanumeric characters, a period or a hyphen for the characters in between, if any.";
+            + "    - consist of alphanumeric characters, a period or a hyphen for the characters in between, if any.\n"
+            + "3. Finally, it is followed by a '.' and then a TLD (Top Level Domain) \n"
+            + "    - be at least 2 characters long\n"
+            + "    - consist of only letters";
     // alphanumeric and special characters
     private static final String LOCAL_PART_REGEX = "^[\\w" + SPECIAL_CHARACTERS + "]+";
     private static final String DOMAIN_FIRST_CHARACTER_REGEX = "[^\\W_]"; // alphanumeric characters except underscore
     private static final String DOMAIN_MIDDLE_REGEX = "[a-zA-Z0-9.-]*"; // alphanumeric, period and hyphen
-    private static final String DOMAIN_LAST_CHARACTER_REGEX = "[^\\W_]$";
+    private static final String DOMAIN_LAST_CHARACTER_REGEX = "[^\\W_]";
+    private static final String TOP_LEVEL_DOMAIN_REGEX = "[a-zA-Z]{2,}$";
     public static final String VALIDATION_REGEX = LOCAL_PART_REGEX + "@"
-            + DOMAIN_FIRST_CHARACTER_REGEX + DOMAIN_MIDDLE_REGEX + DOMAIN_LAST_CHARACTER_REGEX;
+            + DOMAIN_FIRST_CHARACTER_REGEX + DOMAIN_MIDDLE_REGEX + DOMAIN_LAST_CHARACTER_REGEX
+            + "\\." + TOP_LEVEL_DOMAIN_REGEX;
 
     public final String value;
 
